0c3d0ceaf28a674b3cfc2d391c0f1ed7c5fc2f1b
Nicu Reut <nicu.reut@digitalasset.com>
2024-06-10 09:55:34 +0200

    changes from nicureut-da-latitude on Mo 10 Jun 2024 09:55:34 CEST

--- tmux.nix
+++ tmux.nix
@@ -47,7 +47,7 @@
       # increase timeout to select pane
       set -g display-panes-time 4000
       # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
-      set -g default-terminal "xterm-256color"
+      set -g default-terminal "screen-256color"
       #set -ga terminal-overrides ",*256col*:Tc"
       #set -ga terminal-overrides "*:Ss=\E[%p1%d q:Se=\E[ q"
       set-environment -g COLORTERM "truecolor"
