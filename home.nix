a89bc3e70d810482689fe59aef2a3e7d7a7f0b7e
Nicu Reut <contact@nicu.dev>
2024-10-06 09:52:03 +0000

    more mullvad

--- home.nix
+++ home.nix
@@ -41,7 +41,6 @@
     pkgs.bottom
     pkgs.jq
     pkgs.zoxide
-    pkgs.mullvad-vpn
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
--- hosts/xps/home.nix
+++ hosts/xps/home.nix
@@ -1,7 +1,12 @@
 { config, pkgs, ... }:
 {
+  home.services = {
+    mullvad-vpn.enable = true;
+    mullvad-vpn.package = pkgs.mullvad-vpn;
+  };
   home.packages = [
     pkgs.ansible
     pkgs.sshpass
+    pkgs.mullvad-vpn
   ];
 }
