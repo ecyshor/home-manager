9d9b74989938bb6aa78dc7612aaa2789cb6d80ab
Nicu Reut <nicu.reut@digitalasset.com>
2025-12-05 10:23:10 +0100

    changes from nicureut-Precision-5690 on Fri Dec 5 10:23:10 AM CET 2025

--- dconf.nix
+++ dconf.nix
@@ -16,7 +16,6 @@
     "org/gnome/shell" = {
       enabled-extensions = [
         "clipboard-indicator@tudmotu.com"
-        #"Vitals@CoreCoding.com"
         ];
       };
     "org/gnome/desktop/interface".show-battery-percentage = true;
