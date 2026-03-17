69a2002f1dcafb2c26d866f496f7a420d0cfd4de
Nicu Reut <nicu.reut@digitalasset.com>
2026-03-17 14:07:06 +0100

    add grep in directory

--- nvim/neo-tree.nix
+++ nvim/neo-tree.nix
@@ -1,10 +1,48 @@
-{ config, pkgs, ... }:
-
 {
   programs.nixvim = {
     plugins = {
-      neo-tree.enable = true;
+      neo-tree = {
+        enable = true;
+
+        # https://github.com/nvim-neo-tree/neo-tree.nvim/blob/main/lua/neo-tree/defaults.lua
+        settings = {
+
+          window = {
+            mappings = {
+              "g" = "grep_in_directory";
+            };
+          };
+
+          source_selector = {
+            winbar = true;
+          };
+          # Define the custom command
+          commands = {
+            grep_in_directory = {
+              __raw = ''
+              function(state)
+                local node = state.tree:get_node()
+                local path = node.type == 'directory' and node.path or vim.fn.fnamemodify(node.path, ":h")
+
+                -- Ensure telescope is available
+                local has_telescope, builtin = pcall(require, "telescope.builtin")
+                if not has_telescope then
+                  print("Telescope not found")
+                  return
+                end
+
+                builtin.live_grep({
+                  search_dirs = { path },
+                  prompt_title = "Grep in: " .. vim.fn.fnamemodify(path, ":t"),
+                })
+              end
+              '';
+            };
+          };
+        };
+      };
     };
+
     keymaps = [
       {
         action = "<cmd>Neotree toggle<CR>";
